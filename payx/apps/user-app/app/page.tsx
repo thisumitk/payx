6eaf2e777e29196b89743eab0b331fcaec51074d
Sumit <111978673+sumitk-jma@users.noreply.github.com>
2024-10-02 12:34:24 +0530

    check

--- payx/apps/user-app/app/page.tsx
+++ payx/apps/user-app/app/page.tsx
@@ -5,11 +5,11 @@ import { authOptions } from "./lib/auth";
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
-    
+
     redirect('/dashboard')
   } else {
+    
     redirect('/api/auth/signin')
 
-  }
-  
+  } 
 }
